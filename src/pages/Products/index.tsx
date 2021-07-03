d155c8c4cdc53b59409a9a430ff4a4a5a5742bc5
EDUDSF <eduardo.sf.1992@gmail.com>
2021-07-03 13:57:43 -0300

    feat: add pages -> Cart/Checkout/Products

---
+++ src/pages/Products/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart (): JSX.Element {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Cart
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,12 +1,14 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: 'Roboto', sans-serif;
-  text-decoration: none;
-  list-style: none;
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    text-decoration: none;
+    list-style: none;
+  }
 `
 export const Container = styled.div`
   margin: 0
