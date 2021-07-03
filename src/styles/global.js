fa4a0fade25e10cfd47c6e380e8780113266b9d2
EDUDSF <eduardo.sf.1992@gmail.com>
2021-07-03 13:46:02 -0300

    feat: add GlobalStyle

---
+++ src/styles/global.js
@@ -0,0 +1,13 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  text-decoration: none;
+  list-style: none;
+`
+export const Container = styled.div`
+  margin: 0
+`
