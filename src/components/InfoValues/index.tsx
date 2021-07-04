ff422fad1ca6d3e211fada257681d2f4d782ab4e
EDUDSF <eduardo.sf.1992@gmail.com>
2021-07-04 16:02:14 -0300

    feat: create InfoValues diagram

--- src/components/InfoValues/index.tsx
+++ src/components/InfoValues/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import InfoBox from '@/components/CardsContainers'
+import { Container } from './style'
 
 function InfoValues (): JSX.Element {
   return (
-    <>
-      teste
-    </>
+    <InfoBox name="VALORES">
+      <Container>
+        <div>
+          <span>Total</span>
+          <span>(sem impostos)</span>
+          <span>R$ 450,00</span>
+        </div>
+        <div>
+          <span>Impostos</span>
+          <span>R$ 10,00</span>
+        </div>
+        <div>
+          <span>Total:</span>
+          <span>R$ 460,00</span>
+        </div>
+      </Container>
+    </InfoBox>
   )
 }
 
--- src/components/InfoValues/style.js
+++ src/components/InfoValues/style.js
@@ -1,5 +1,41 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  
+  display: block;
+  overflow: hidden;
+  text-align: center;
+  font-size: 0.9rem;
+  color: #555;
+  & > * {
+    & > :last-child {
+      padding-top: 4px;
+      font-weight: 600;
+      color: #000;
+      font-size: 1rem;
+    }
+  }
+  & > :first-child {
+    float: left;
+    width: 50%;
+    & > * {
+      display: block;
+    }
+  }
+  & > :nth-child(2) {
+    width: 50%;
+    float: right;
+    & > * {
+      display: block;
+    }
+  }
+  & > :last-child {
+    margin-top: 20px;
+    display: block;
+    width: 100%;
+    float: left;
+    flex-direction: column;
+    & > * {
+      display: block;
+    }
+  }
 `
